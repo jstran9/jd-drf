75d6433ddfbbb79a9983b592635548d06ed85dc4
ttran9 <toddtran9@gmail.com>
2020-08-02 11:50:13 -0700

    adding endpoints to login as well as return token to logged in user.

--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token # view to return token to user when they log-in.
 
 from core.views import TestView
 
@@ -7,5 +8,7 @@ from core.views import TestView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', TestView.as_view(), name='test')
+    path('', TestView.as_view(), name='test'),
+    path('api/token/', obtain_auth_token, name = 'obtain-token'),
+    path('rest-auth/', include('rest_auth.urls'))
 ]
