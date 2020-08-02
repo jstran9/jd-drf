15e006c3879309d1696c8ea4bcffde98e82f1c94
ttran9 <toddtran9@gmail.com>
2020-08-02 14:28:00 -0700

    adding endpoints to display views.

--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token # view to return token to user when they log-in.
 
-from core.views import TestView
+from core.views import PostView, PostCreateView, PostListCreateView
 
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', TestView.as_view(), name='test'),
+    path('', PostView.as_view(), name='test'),
+    path('create/', PostCreateView.as_view(), name='create'),
     path('api/token/', obtain_auth_token, name = 'obtain-token'),
+    path('list-create/', PostListCreateView.as_view(), name='list-create'),
     path('rest-auth/', include('rest_auth.urls'))
 ]
